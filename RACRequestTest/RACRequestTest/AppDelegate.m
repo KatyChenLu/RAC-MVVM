60581f873c3ba439dbc12c843263181dc9e778f2
lul <630001384@qq.com>
2017-03-20 09:47:13 +0800

    去掉test页

--- RACRequestTest/RACRequestTest/AppDelegate.m
+++ RACRequestTest/RACRequestTest/AppDelegate.m
@@ -40,7 +40,6 @@
     
     UINavigationController *navC1 = [[UINavigationController alloc] initWithRootViewController:oneVC];
     UINavigationController *navC2 = [[UINavigationController alloc] initWithRootViewController:twoVC];
-    UINavigationController *navC3 = [[UINavigationController alloc] initWithRootViewController:thireVC];
     
     
     
