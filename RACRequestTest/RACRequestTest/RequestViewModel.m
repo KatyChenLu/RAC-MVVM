fe1d19b82e942b9f01c6f3a62da96d53ba0dfd9b
lul <630001384@qq.com>
2017-05-12 13:08:03 +0800

    虎虎

--- RACRequestTest/RACRequestTest/RequestViewModel.m
+++ RACRequestTest/RACRequestTest/RequestViewModel.m
@@ -24,7 +24,7 @@
        RACSignal *singal = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
            
            NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
-           parameters[@"q"] = @"relax";
+           parameters[@"q"] = @"虎虎";
            [[HttpManager sharedHttpManager] sendRequestWithDic:parameters opt:OPT_GET shortURL:@"v2/book/search" setSuccessBlock:^(NSDictionary *responseDic) {
                // 请求成功调用
                // 把数据用信号传递出去
