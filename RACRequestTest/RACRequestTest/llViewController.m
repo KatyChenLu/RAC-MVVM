789f08443011b35a6bf527ed6af2cb7148c82c01
lul <630001384@qq.com>
2017-10-13 15:24:22 +0800

    33

--- RACRequestTest/RACRequestTest/llViewController.m
+++ RACRequestTest/RACRequestTest/llViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "llViewController.h"
-
-@interface llViewController ()
-
+#import "BBDragTable.h"
+@interface llViewController ()<BBDragTableDelegate,BBDragTableDataSource>
+@property (nonatomic, strong)BBDragTable *tableView;
+@property (nonatomic, strong)NSMutableArray *data;
 @end
 
 @implementation llViewController
@@ -19,10 +20,46 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.tableView = [[BBDragTable alloc] initWithFrame:CGRectMake(0, 0, 300, 500)];
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self; [self.view addSubview:self.tableView];
+    self.data = [NSMutableArray array];
+    for (int i = 0; i<10; i++) {
+        [self.data addObject:[NSString stringWithFormat:@"%ld",i]];
+    }
+    
+    [self.tableView reloadData];
+    
+   
+}
+- (NSArray *)dataArrayForTableView:(BBDragTable *)tableView {
+    return self.data;
+}
+#pragma mark - UITableViewDelegate
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+   
+    
+    static NSString *CellIdentifier = @"Cell";
+
+    UITableViewCell *cell = (UITableViewCell*)[tableView  dequeueReusableCellWithIdentifier:CellIdentifier];
+
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithFrame:CGRectZero];
+    }
+    cell.textLabel.text = self.data[indexPath.row];
+    return cell;
 }
 
-#pragma mark - UITableViewDelegate
 
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+   return  self.data.count;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 10;
+}
 #pragma mark - Public
 
 #pragma mark - Private
