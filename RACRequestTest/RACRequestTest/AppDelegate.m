a85e0eb315c57fd45459e7eb96c9ad8a31d9a7fc
lul <630001384@qq.com>
2017-10-18 16:37:18 +0800

    11

--- RACRequestTest/RACRequestTest/AppDelegate.m
+++ RACRequestTest/RACRequestTest/AppDelegate.m
@@ -42,8 +42,6 @@
     thireVC.title = @"table";
     fourVC.title = @"draw";
     
-    
-
     UINavigationController *navC1 = [[UINavigationController alloc] initWithRootViewController:oneVC];
     UINavigationController *navC2 = [[UINavigationController alloc] initWithRootViewController:twoVC];
     UINavigationController *navC3 = [[UINavigationController alloc] initWithRootViewController:thireVC];
